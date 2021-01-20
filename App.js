b2c838cc80c2730b939355fda61979422d5b9537
Ben Verbeken <ben@seats.io>
2021-01-20 20:09:36 +0100

    webview

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {WebView} from 'react-native-webview';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <WebView
+          source={{ html: '<div id="chart" style="border: 1px solid red"></div>' }}
+      />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
