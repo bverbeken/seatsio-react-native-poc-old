5d6458252a8dea13acf7d8d6bee02067d1613f93
Ben Verbeken <ben@seats.io>
2021-01-21 09:57:11 +0100

    refactoring

--- src/SeatsioSeatingChart.js
+++ src/SeatsioSeatingChart.js
@@ -1,7 +1,42 @@
-import Embeddable from "./Embeddable";
+import {WebView} from "react-native-webview";
+import React from "react";
 
-export default class SeatsioSeatingChart extends Embeddable {
+export default class SeatsioSeatingChart extends React.Component {
 
+    render() {
+        const html = `
+        <html lang="en">
+        <head>
+            <title>seating chart</title>
+            <script src="${this.props.chartJsUrl}"></script>
+        </head>
+        <body>
+            <div id="${this.props.divId}"></div>
+            <script>
+                new seatsio.SeatingChart({
+                    divId: "${this.props.divId}",
+                    workspaceKey: "${this.props.workspaceKey}",
+                    event: "${this.props.event}"
+                }).render();
+            </script>
+        </body> 
+        </html>
+        `
+
+        console.log(html)
+
+        return (
+            <WebView
+                originWhitelist={['*']}
+                source={{html: html}}
+            />
+        );
+    }
 
 
 }
+
+SeatsioSeatingChart.defaultProps = {
+    divId: 'chart',
+    chartJsUrl: 'https://cdn.seatsio.net/chart.js'
+};
