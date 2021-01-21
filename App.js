11116695c1a56ef9d75dfc278a59a462a7d6d5fd
Ben Verbeken <ben@seats.io>
2021-01-21 10:28:14 +0100

    refactoring

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import SeatsioSeatingChart from "./src/SeatsioSeatingChart";
+import { StyleSheet, View } from 'react-native';
 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 100,
+        backgroundColor: 'red',
+    },
+});
 
 export default function App() {
     return (
-        <SeatsioSeatingChart
-            workspaceKey="publicDemoKey"
-            event="smallTheatreEvent"
-        />
+        <View style={styles.container}>
+            <SeatsioSeatingChart
+                workspaceKey="publicDemoKey"
+                event="smallTheatreEvent"
+            />
+        </View>
     )
 
 }
