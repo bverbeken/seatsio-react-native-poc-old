4e92fbeb3d4c62393253bc0076d3141ae9ce07d7
Ben Verbeken <ben@seats.io>
2021-01-22 10:08:36 +0100

    refactoring

--- src/SeatsioSeatingChartConfig.js
+++ src/SeatsioSeatingChartConfig.js
@@ -7,11 +7,8 @@ export default class SeatsioSeatingChartConfig {
         let config = {
             divId: this.props.divId,
             workspaceKey: this.props.workspaceKey,
-        }
-        if (this.props.event) {
-            config.event = this.props.event
-        } else if (this.props.events) {
-            config.events = this.props.events
+            event: this.props.event,
+            events: this.props.events
         }
         return this.addCallbacks(config);
     }
